20181880b4f54467f5c140bd729a685bb3bbcd92
U-janacek\mdulaney <mdulaney@janacek.(none)>
2013-10-06 11:07:37 -0400

    Refactored the configuration information to support JSON

--- src/logger-client.go
+++ src/logger-client.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "bufio"
+//    "encoding/json"
     "io"
     "flag"
 	"fmt"
@@ -14,6 +15,12 @@ type client struct {
     id int
     ch chan string
     done chan int
+}
+
+type config struct {
+    numOfClients int `json:"numofclients"`
+    server string `json:"server"`
+    inFile string `json:"inputfile"`
 }
 
 func FileReader(fileName string, clients []client, frDone chan bool) {
@@ -86,29 +93,24 @@ func LogReporterClient(c client, addr string, frDone chan bool) {
 }
 
 func main() {
-    var inFileName string
-    var addr string
 
-	flag.StringVar(&addr, "t", "127.0.0.1:50000", "<ip>:<port>")
-    flag.StringVar(&inFileName, "f", "data/infile.txt", "input file")
+    cfg := config { } 
+
+	flag.StringVar(&cfg.server, "t", "127.0.0.1:50000", "<ip>:<port>")
+    flag.StringVar(&cfg.inFile, "f", "data/infile.txt", "input file")
+    flag.IntVar(&cfg.numOfClients, "n", 3, "number of clients")
 
     flag.Parse()
 
     clients := make([]client, 0)
     frDone := make(chan bool)
 
-    chan1 := make(chan string)
-    done1 := make(chan int)
-    clients = append(clients, client{ 0, chan1 , done1})
+    for i := 0; i < cfg.numOfClients; i++ {
+        clients = append(clients, client{i, make(chan string), make(chan int)})
+        go LogReporterClient(clients[i], cfg.server, frDone)
+    }
 
-    chan2 := make(chan string)
-    done2 := make(chan int)
-    clients = append(clients, client{ 0, chan2 , done2})
-
-    go LogReporterClient(clients[0], addr, frDone)
-    go LogReporterClient(clients[1], addr, frDone)
-
-    go FileReader(inFileName, clients, frDone)
+    go FileReader(cfg.inFile, clients, frDone)
 
     for _, c := range clients {
         <-c.done
