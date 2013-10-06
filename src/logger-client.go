3fc693220839814ef3715521db4bb066661e71b7
U-janacek\mdulaney <mdulaney@janacek.(none)>
2013-10-06 10:13:52 -0400

    Now reads from a file and sends the output to the server

--- src/logger-client.go
+++ src/logger-client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "bufio"
+    "io"
     "flag"
 	"fmt"
 	"log"
@@ -10,11 +12,11 @@ import (
 
 type client struct {
     id int
-    c chan string
+    ch chan string
     done chan int
 }
 
-func FileReader(fileName string) {
+func FileReader(fileName string, outChan chan string, frDone chan bool) {
     fmt.Printf("Opening file %s\n", fileName)
 
     f, err := os.Open(fileName)
@@ -25,10 +27,26 @@ func FileReader(fileName string) {
 
     defer f.Close()
 
+    r := bufio.NewReader(f)
+
+    for {
+        line, err := r.ReadString('\n')
+
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            log.Fatal(err)
+        }
+
+        outChan <-line
+    }
+
+    fmt.Printf("Finished reading file\n")
+    frDone<-true
 }
 
 // TODO: can go routines return values?
-func LogReporterClient(c client, addr string) {
+func LogReporterClient(c client, addr string, frDone chan bool) {
 	fmt.Printf("Connecting to %s\n", addr)
 
 	conn, err := net.Dial("tcp", addr)
@@ -37,22 +55,26 @@ func LogReporterClient(c client, addr string) {
 		log.Fatal(err)
 	}
 
-	bytesWritten, err := conn.Write([]byte("Here's a message2\n"))
+	defer conn.Close()
 
-	if err != nil {
-		log.Fatal(err)
-	}
+    isDone := false
 
-	bytesWritten, err = conn.Write([]byte("a\n"))
+    for ; !isDone; {
+        select {
+        case data := <-c.ch:
+	        bytesWritten, err := conn.Write([]byte(data))
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	        if err != nil {
+                log.Fatal(err)
+	        }
+	        fmt.Printf("Sent %d bytes\n", bytesWritten)
+        case <- frDone:
+            fmt.Printf("Received terminate signal\n")
+            isDone = true
+        }
 
-	fmt.Printf("Sent %d bytes\n", bytesWritten)
-
-	conn.Close()
-
+    }
+    fmt.Printf("Notifying main\n")
     c.done <- 1
 }
 
@@ -62,6 +84,7 @@ func main() {
 
     chan1 := make(chan string)
     done1 := make(chan int)
+    frDone := make(chan bool)
 
     cli := client{ 0, chan1 , done1}
 
@@ -70,9 +93,9 @@ func main() {
 
     flag.Parse()
 
-    go FileReader(inFileName)
+    go FileReader(inFileName, chan1, frDone)
 
-    go LogReporterClient(cli, addr)
+    go LogReporterClient(cli, addr, frDone)
 
     <-cli.done
 }
